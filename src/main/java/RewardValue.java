2b056bba3b0f410aa9f4cc7dcab369c2172ffbe9
Shelton Mutambirwa <sheltonmutambirwa@gmail.com>
2024-09-25 19:36:02 +0200

    Created the RewardValue class

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,22 @@
+
+public class RewardValue {
+    double cash;
+    int miles;
+    public RewardValue(double cashValue){
+        cash = cashValue;
+    }
+    public RewardValue(int mileValue){
+        miles = mileValue;
+    }
+    public double getCashValue(){
+        if(cash != 0){return cash;}
+        else{cash = (double) ( miles/ 0.0035);
+        return cash;}
+    }
+    public int getMilesValue(){
+        if(miles != 0){return miles;}
+        else{miles = (int) (cash*0.0035);
+        return miles;}
+    }
+
+}
